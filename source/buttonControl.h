1e1b5c9ed9f1edccebb79231def1d7e0244fb554
ELBEBACK <elbekbegaliev305@gmail.com>
2026-02-06 02:10:43 +0300

    button interruption added

--- source/buttonControl.h
+++ source/buttonControl.h
@@ -1,57 +1,48 @@
-#include "esp32-hal-gpio.h"
 #ifndef BUTTON_H
 #define BUTTON_H
 
 class ButtonControl {
-
     int IN_PIN;
-
-    bool stableState = HIGH;
-    bool lastReading = HIGH;
+    volatile bool stableState = HIGH;
     bool prevStableState = HIGH;
-    unsigned long lastChangeMs = 0;
-    const unsigned long debounceMs = 40;
+    
+    // Static member pointing to instance
+    static ButtonControl* instance;
 
-public: 
+public:
     ButtonControl(int pin) : IN_PIN(pin) {
         pinMode(pin, INPUT_PULLUP);
+        instance = this;  // Save current instance
+    }
+
+    static void IRAM_ATTR buttonInterrupt() {
+        if (instance) {
+            instance->stableState = digitalRead(instance->IN_PIN);
+        }
     }
 
     void begin() {
         stableState = digitalRead(IN_PIN);
-        lastReading = stableState;
         prevStableState = stableState;
-        lastChangeMs = millis();
-    }
-
-    void update() {
-        bool r = digitalRead(IN_PIN);
-
-        if (r != lastReading) {
-        lastReading = r;
-        lastChangeMs = millis();
-        }
-
-        if (millis() - lastChangeMs >= debounceMs) {
-        stableState = lastReading;
-        }
-    }    
-
-    bool isPressed() const {
-        return stableState == LOW;
+        attachInterrupt(digitalPinToInterrupt(IN_PIN), buttonInterrupt, CHANGE);
     }
 
     bool wasPressed() {
-        update(); 
-
         bool pressedEdge = (prevStableState == HIGH && stableState == LOW);
         prevStableState = stableState;
         return pressedEdge;
+    }
+
+    bool isPressed() const {
+        return stableState == LOW;
     }
 
     bool getButtonState() {
         return !digitalRead(IN_PIN);
     }
 };
+
+// Initialize static member outside the class
+ButtonControl* ButtonControl::instance = nullptr;
 
 #endif
